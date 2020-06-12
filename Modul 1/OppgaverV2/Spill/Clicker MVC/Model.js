6f8fc9e146a44e7d613a0f9642e6d6e54781dd06
mrskme <mrskme@hotmail.no>
2020-06-12 20:22:20 +0200

    kjnk

--- Modul 1/OppgaverV2/Spill/Clicker MVC/Model.js
+++ Modul 1/OppgaverV2/Spill/Clicker MVC/Model.js
@@ -4,6 +4,7 @@ let model = {
     whichClicking: 1,
     price: 20,
     hide: 'none',
+    buttonText: 'Upgrade mousepower',
     counting: function() {
         this.points = this.points + this.pointsUpgrade, 
          updateView()
@@ -25,48 +26,56 @@ let model = {
         this.clickerUpgrade1.apply(this),
         this.pointsUpgrade = 5, 
         this.price = 150
+        this.buttonText = 'MORE MOUSEPOWER!'
         console.log("upgrade2")
     },
     clickerUpgrade3: function() {
-        this.clickerUpgrade2.apply(this),
+        this.clickerUpgrade1.apply(this),
         this.pointsUpgrade= 12, 
         this.price = 400
+        this.buttonText = 'MWHAHAHAHAHAHA!!'
         console.log("upgrade3")
     },
     clickerUpgrade4: function() {
-        this.clickerUpgrade3.apply(this),
+        this.clickerUpgrade1.apply(this),
         this.pointsUpgrade = 31, 
         this.price = 800
+        this.buttonText = '<div style="font-size: 18px">MWWWWOHAHAHAHAH!!<div>',
         console.log("upgrade4") 
     },
     clickerUpgrade5: function () {
-        this.clickerUpgrade4.apply(this),
+        this.clickerUpgrade1.apply(this),
         this.pointsUpgrade = 82,
         this.price = 1400
+        this.buttonText = 'Upgrade mousepower (i\'m done now)'
         console.log("upgrade5")
     },
     clickerUpgrade6: function () {
-        this.clickerUpgrade4.apply(this),
+        this.clickerUpgrade1.apply(this),
         this.pointsUpgrade = 202,
         this.price = 2800
+        this.buttonText = 'JUST KIDDING! MWHAHAHAHAHA!'
         console.log("upgrade6")
     },
     clickerUpgrade7: function () {
-        this.clickerUpgrade4.apply(this),
+        this.clickerUpgrade1.apply(this),
         this.pointsUpgrade = 391,
         this.price = 5600
+        this.buttonText = 'YOU THINK YOU CAN CHALLANGE ME MORTAL?'
         console.log("upgrade7")
     },
     clickerUpgrade8: function () {
-        this.clickerUpgrade4.apply(this),
+        this.clickerUpgrade1.apply(this),
         this.pointsUpgrade = 893,
         this.price = 11200,
+        this.buttonText = 'I AM A GOD, I CAN DO THIS FOREVER!'
         console.log("upgrade8")
     },
     clickerUpgrade9: function () {
         this.clickerUpgrade4.apply(this),
         this.pointsUpgrade = 1999,
         this.price = 19999,
+        this.buttonText = 'MWAHAHAHAHAHA'
         console.log("upgrade9")
     },
 }
--- Modul 1/OppgaverV2/Spill/Clicker MVC/View.js
+++ Modul 1/OppgaverV2/Spill/Clicker MVC/View.js
@@ -1,9 +1,9 @@
 function updateView() {
     document.getElementById('content').innerHTML = `
-        <button id="clicker" onclick="model.counting(), model.hideOrNot1(), consolelog()">Click me</button>
+        <button id="clicker" onclick="model.counting(), model.hideOrNot1(), updateView(), consolelog()">Suspicious button</button>
         <div class="points">${model.points}</div>
         <div>+${model.pointsUpgrade}</div>
         </br>
-        <button class="tooltip" id="Upgrade1" style="display:${model.hide}" onclick="model.clickerUpgrade${model.whichClicking}(), updateView()";>Upgrade mousepower<span class="tooltiptext">Price: ${model.price}</span></button>
+<button class="tooltip" id="Upgrade1" style="display:${model.hide}" onclick="model.clickerUpgrade${model.whichClicking}(), updateView()";>${model.buttonText}<span class="tooltiptext">Price: ${model.price}</span></button>
     `;
 };
